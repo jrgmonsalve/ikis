925f36a94eeea7a019df74fb17e511ac7b37e9fe
Johan Garcia <jgarcia@blossom.net>
2026-06-06 09:39:03 -0500

    fix: configure Firebase environments

--- apps/web/src/environments/environment.ts
+++ apps/web/src/environments/environment.ts
@@ -1,11 +1,18 @@
 export const environment = {
+  useEmulators: true,
   production: false,
   firebase: {
-    apiKey: 'REPLACE_WITH_FIREBASE_API_KEY',
-    authDomain: 'REPLACE_WITH_FIREBASE_AUTH_DOMAIN',
-    projectId: 'REPLACE_WITH_FIREBASE_PROJECT_ID',
-    storageBucket: 'REPLACE_WITH_FIREBASE_STORAGE_BUCKET',
-    messagingSenderId: 'REPLACE_WITH_FIREBASE_MESSAGING_SENDER_ID',
-    appId: 'REPLACE_WITH_FIREBASE_APP_ID',
+    apiKey: 'AIzaSyB91Q0yVJz1J-e-KUk2YG4bUTHuQt1Jywk',
+    authDomain: 'ikis-5fed9.firebaseapp.com',
+    projectId: 'ikis-5fed9',
+    storageBucket: 'ikis-5fed9.firebasestorage.app',
+    messagingSenderId: '802368966666',
+    appId: '1:802368966666:web:fbf4ae34870c313135cff3',
+    measurementId: 'G-E8LRT7BK83',
+  },
+  emulators: {
+    auth: { host: 'localhost', port: 9099 },
+    firestore: { host: 'localhost', port: 8081 },
+    functions: { host: 'localhost', port: 5001 },
   },
 };
